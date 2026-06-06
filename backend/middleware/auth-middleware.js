4d3a9ca2d73aa6db7a99220b03b98d2434f06a09
sarvaiya-sunil <sarvaiyasunil25@gmail.com>
2026-06-06 11:37:57 +0530

    getproduct route created with authorization

---
+++ backend/middleware/auth-middleware.js
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user-model.js";
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    const accessToken = req.cookies.accessToken;
+    if (!accessToken) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized - No access token provided" });
+    }
+    try {
+      const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+      const user = await User.findById(decoded.userId);
+      if (!user) {
+        return res.status(401).json({ message: "User not found" });
+      }
+      req.user = user;
+      next();
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        return res.status(401).json({ message: "Access token expired" });
+      }
+      throw error;
+    }
+  } catch (error) {
+    res.status(401).json({ message: "Invalid access token" });
+  }
+};
+
+export const adminRoute = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    return res.status(403).json({ message: "Access denied - Admin only" });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth-route.js";
+import productRoutes from "./routes/product-route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // allows you to parse body of the request
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port http://localhost:" + PORT);
