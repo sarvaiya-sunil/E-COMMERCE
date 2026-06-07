6dbb40901858432b0b62e6b7af944fe3c8783f84
sarvaiya-sunil <sarvaiyasunil25@gmail.com>
2026-06-07 18:21:16 +0530

    analytics route completed

--- backend/routes/auth-route.js
+++ backend/routes/auth-route.js
@@ -4,7 +4,9 @@ import {
   logout,
   signup,
   refreshToken,
+  getProfile,
 } from "../controllers/auth-controller.js";
+import { protectRoute } from "../middleware/auth-middleware.js";
 
 const router = express.Router();
 
@@ -12,6 +14,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.post("/refresh-token", refreshToken);
+router.post("/get-profiel", protectRoute, getProfile);
 
 export default router;
 
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import productRoutes from "./routes/product-route.js";
 import cartRoutes from "./routes/cart-route.js";
 import couponRoutes from "./routes/coupon-route.js";
 import paymentRoutes from "./routes/payment-route.js";
+import analyticsRoutes from "./routes/analytics-route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -19,6 +20,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port http://localhost:" + PORT);
